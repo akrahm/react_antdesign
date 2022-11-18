1e796cf1541b43901ea55e0aa5f9ac2800849e17
vishal <vishal.kannoujiya@ongraphmail.com>
2022-11-18 16:47:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Layout, Typography, Avatar, Menu, Breadcrumb } from 'antd';
+import User from "./images/hacker.png";
+import 'antd/dist/reset.css';
+const { Title } = Typography;
+const { Header, Footer, Sider, Content } = Layout;
+
 
 function App() {
+
+  let arr = [
+    {
+      key: '1',
+      label: 'nav 1',
+    },
+    {
+      key: '2',
+      label: 'nav 2',
+    },
+    {
+      key: '3',
+      label: 'nav 3',
+    },
+    {
+      key: '4',
+      label: 'nav 4',
+    },
+    {
+      key: '5',
+      label: 'nav 5',
+    },
+    {
+      key: '6',
+      label: 'nav 6',
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{padding: "10px"}}>
+          <Avatar style={{ float: "right" }} src={User} />
+        <Title style={{ color: "white" }} level={3}>visHAL</Title>
+        </Header>
+        <Layout>
+          <Sider>
+          <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={arr}
+        />
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content style={{backgroundColor:"#fff", margin: 0, padding: 24, height: "76vh"}}>Content</Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          </Layout>
+          </Layout>
+    </Layout>
     </div>
   );
 }
